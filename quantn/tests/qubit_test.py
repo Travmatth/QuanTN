a35e9b8c4b43a0085358ba8c6654ca704ae501e8
Travis Matthews <trav.matth@gmail.com>
2020-01-28 20:16:08 -0800

    more rigorous bitstring testing

--- quantn/tests/qubit_test.py
+++ quantn/tests/qubit_test.py
@@ -1,12 +1,18 @@
+import numpy as np
 from quantn import Qubit
 
 def test_qubit_init():
   q_0 = Qubit()
-  print(q_0.shape)
   assert q_0.shape[0] == 2
 
 def test_qubit_0ket_bitstring():
   q_0 = Qubit()
-  for i in range(100):
+  for i in range(1000):
     assert q_0.bitstring() == "0"
+
+def test_qubit_1ket_bitstring():
+  q_0 = Qubit()
+  q_0.set_tensor(np.array([0, 1]))
+  for i in range(1000):
+    assert q_0.bitstring() == "1"
 
